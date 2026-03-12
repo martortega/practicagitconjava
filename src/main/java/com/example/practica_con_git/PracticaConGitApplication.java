2959695d4f7e57b3d79d8fc3b7e9300eb0852bc2
Ruben <ruben.diaz.200080@gmail.com>
2026-03-11 19:00:20 -0600

    Se agrega una nueva funcion

--- src/main/java/com/example/practica_con_git/PracticaConGitApplication.java
+++ src/main/java/com/example/practica_con_git/PracticaConGitApplication.java
@@ -9,6 +9,11 @@ public class PracticaConGitApplication {
 	public String saludar(){
 		return "Hola mundo!";
 	}
+
+	public String saludarConBuilder(){
+		StringBuilder sb = new StringBuilder();
+		return sb.append("Hola con builder").toString();
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PracticaConGitApplication.class, args);
