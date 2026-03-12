1d861aab9b4dfc4d5e1fb4a1867d99cd79cbdfbf
A30_Martin Antonio Ortega Yepez <tinmarorye@gmail.com>
2026-03-11 18:22:48 -0600

    mi primer proyecto java

---
+++ src/test/java/com/example/practica_con_git/PracticaConGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practica_con_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaConGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
